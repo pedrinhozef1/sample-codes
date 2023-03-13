a02f9d2786739bed1f0574bbc31e5d98606db895
pedrinhozef1 <pedrolucas.7@hotmail.com>
2023-03-13 19:11:18 -0300

    Aplicado o pattern State

--- patterns-01/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
+++ patterns-01/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
@@ -1,30 +1,44 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
-    private String situacao;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens){
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
     public void aplicarDescontoExtra() {
-        BigDecimal valorDescontoExtra = BigDecimal.ZERO;
-
-        if (situacao.equals("EM_ANALISE")) {
-            valorDescontoExtra = new BigDecimal("0.05");
-        } else if (situacao.equals("APROVADO")) {
-            valorDescontoExtra = new BigDecimal("0.02");
-        }
+        BigDecimal valorDescontoExtra = this.situacao.calcularDescontoExtra(this);
         this.valor = this.valor.subtract(valorDescontoExtra);
     }
 
     public void aprovar() {
-        this.situacao = "APROVADO";
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return this.situacao;
     }
 
     public BigDecimal getValor() {

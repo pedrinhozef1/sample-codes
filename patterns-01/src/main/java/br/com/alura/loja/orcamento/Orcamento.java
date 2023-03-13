4d238256dfc6a4dde8ac8c3fa339efbf3e647e66
pedrinhozef1 <pedrolucas.7@hotmail.com>
2023-03-13 16:44:45 -0300

    Adicionado funcao de desconto extra e situacao do orcamento

--- patterns-01/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
+++ patterns-01/src/main/java/br/com/alura/loja/orcamento/Orcamento.java
@@ -5,10 +5,26 @@ import java.math.BigDecimal;
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens){
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDescontoExtra = BigDecimal.ZERO;
+
+        if (situacao.equals("EM_ANALISE")) {
+            valorDescontoExtra = new BigDecimal("0.05");
+        } else if (situacao.equals("APROVADO")) {
+            valorDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao = "APROVADO";
     }
 
     public BigDecimal getValor() {

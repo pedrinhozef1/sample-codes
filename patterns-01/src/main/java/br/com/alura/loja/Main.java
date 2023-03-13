a78929e8e7a815c7e8e3b44b601f24a638b8b7f2
pedrinhozef1 <pedrolucas.7@hotmail.com>
2023-03-13 09:25:06 -0300

    Adicionado projeto Loja com calculo de impostos

--- patterns-01/src/main/java/br/com/alura/loja/Main.java
+++ patterns-01/src/main/java/br/com/alura/loja/Main.java
@@ -1,7 +1,17 @@
-package br.com.alura;
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.TipoImposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+
+        CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
+
+        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
     }
 }

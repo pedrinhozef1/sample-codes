8a447ced62c731cc79b3e2d02c2b414852acbbeb
pedrinhozef1 <pedrolucas.7@hotmail.com>
2023-03-13 20:10:10 -0300

    Adicionado simulacao de uma chamada para api externa

---
+++ patterns-01-02/src/main/java/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,8 @@
+package br.com.alura.loja.orcamento;
+
+public class RegistroDeOrcamento {
+    public void registrar(Orcamento orcamento) {
+        // realizar chamada HTTP para uma api externa
+        System.out.println("Chamando api externa...");
+    }
+}
